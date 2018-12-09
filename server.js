406a4111ab9791f7d48901ef4b523ba05311e153
Raghava Lakshminarayana <raglaks94@gmail.com>
2018-12-09 17:18:44 -0600

    add desc for each article

--- server.js
+++ server.js
@@ -13,13 +13,27 @@ axios.request({
 
     //console.log(response.data);
 
+    //Summary - a short summary of the article
+
+    //* URL - the url to the original article
+
+    //* Feel free to add more content to your database(photos, bylines, and so on).
+
     const $ = cheerio.load(response.data);
 
     $("h1").each((i, element) => {
 
         let title = $(element).text();
 
-        console.log(title);
+        //console.log(`${title}\n`);
+
+    });
+
+    $("div.content").each((i, element) => {
+
+        let desc = $(element).text();
+
+        console.log(`${desc}\n`);
 
     });
 
