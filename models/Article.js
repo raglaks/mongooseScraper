eb65133e48974cac2c90ad09b0b28e086dc5f22a
Raghava Lakshminarayana <raglaks94@gmail.com>
2018-12-10 21:00:42 -0600

    add article model

---
+++ models/Article.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const ArticleSchema = new Schema({
+  
+  headline: {
+    type: String,
+    required: true
+  },
+  
+  url: {
+    type: String,
+    required: true
+  },
+
+  desc: {
+    type: String,
+    required: true
+  },
+  
+  comment: {
+    type: Schema.Types.ObjectId,
+    ref: "Comment"
+  }
+
+});
+
+
+const Article = mongoose.model("Article", ArticleSchema);
+
+module.exports = Article;
