618faaa842cadaaf4b1496c00ad47904cc0122af
Raghava Lakshminarayana <raglaks94@gmail.com>
2018-12-10 20:39:01 -0600

    code refactor; scrape from same div to avoid future errors

--- server.js
+++ server.js
@@ -17,8 +17,25 @@ axios.request({
 
 }).then((response) => {
 
+    //console.log(response.data);
+
     const $ = cheerio.load(response.data);
 
+    $("div.FeedCard").each((i, element)=> {
+
+        let impDiv = $(element).children("div.CardHeadline");
+
+        let head = $(impDiv).children("a.headline");
+
+        let link = $(head).attr("href");
+
+        let title = $(head).children("h1").text();
+
+        console.log(`\nTITLE: ${title}\n\nLINK: ${link}\n`);
+
+    });
+
+    //headlines
     $("h1").each((i, element) => {
 
         let title = $(element).text();
@@ -31,49 +48,49 @@ axios.request({
 
     });
 
-    $("div.content").each((i, element) => {
+    //blurbs
+    // $("div.content").each((i, element) => {
 
-        let desc = $(element).children("p").text();
+    //     let desc = $(element).children("p").text();
 
-        // console.log(`DESC: ${desc}\n`);
+    //     console.log(`DESC: ${desc}, ${i}\n`);
 
-        // console.log(i);
+    //     blurbs.push(desc);
 
-        blurbs.push(desc);
+    // });
 
-    });
-
+    //links
     $("a.headline").each((i, element) => {
 
         let lenk = $(element).attr("href");
 
-        console.log(`LENK: ${lenk}\n`);
+        //console.log(`LENK: ${lenk}\n`);
 
         // console.log(i);
 
-        const reg = /\/afs:Content:/g;
+        urls.push(lenk);
 
-        console.log(); 
+        // const reg = /\/afs:Content:/g;
 
-        if (reg.test(lenk) === true) {
+        // if (reg.test(lenk) === true) {
 
-            urls.push("PHOTOS");
+        //     urls.push("PHOTOS");
 
-        } else {
+        // } else {
 
-            urls.push(lenk);
+        //     urls.push(lenk);
 
-        }
+        // }
 
     });
 
     heads.splice(0, 1);
 
-    console.log(heads.length);
+    // console.log(`\nHEADLINES (${heads.length}):\n ${heads}`);
 
-    console.log(blurbs.length);
+    // console.log(`\nBLURBS (${blurbs.length}):\n ${blurbs}`);
 
-    console.log(urls.length);
+    // console.log(`\nURLS (${urls.length}):\n ${urls}`);
 
 }).catch((err) => {
 
