d2480842cc36a0050c3811cfb26e6adfc57517fd
Raghava Lakshminarayana <raglaks94@gmail.com>
2019-01-06 19:38:38 -0600

    add env var for heroku deploy

--- server.js
+++ server.js
@@ -21,7 +21,9 @@ const exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-mongoose.connect("mongodb://localhost/APscrape", { useNewUrlParser: true });
+let MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/APscrape";
+
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
 app.get("/home", function (req, res) {
 
